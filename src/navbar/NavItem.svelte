c9042a906e9ec9572c53d05a42a2382e7ce0dac7
Ben <bfb3ab@virginia.edu>
2020-09-12 15:10:19 -0400

    test change

---
+++ src/navbar/NavItem.svelte
@@ -0,0 +1,26 @@
+<style>
+    div {
+        height: 4rem;
+		width: 8rem;
+        background-color: white;
+        color: #FF9933;
+        border-left: 0.6rem solid black;
+        border-right: 0.6rem solid black;
+        padding: 1rem;
+        background-image: linear-gradient(45deg, #FF9933 50%, transparent 50%);
+        background-position: 100%;
+        transition: background 300ms ease-in-out;
+    }
+    div:hover{
+        background-position: 0;
+		color: #FF9933; 
+    }
+</style>
+
+<script>
+    export let title;
+</script>
+
+<div>
+    <p>{title}</p>
+</div>
---
+++ src/navbar/Navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+    import NavItem from './NavItem.svelte';
+    let items = ['browse', 'home', 'account']
+</script>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: row;
+        justify-content:space-around;
+        align-items: center;
+    }
+</style>
+
+<div>
+    {#each items as item}
+        <NavItem title={item} />
+    {/each}
+</div>
